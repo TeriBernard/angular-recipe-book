bbd5e5914ebe57c1a99f9d6457d2d38ad830874a
Teri <teri@bernardtechpartners.com>
2017-06-30 08:02:00 -0400

    set up recipe model and recipe-list component.ts

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pesto', 'Italian Green Goodness', 'https://cdn.pixabay.com/photo/2016/11/13/21/43/pesto-1822124_960_720.jpg')
+  ];
 
   constructor() { }
 
