7fd7a31c1f6c020ec4d1e6b05b5d457b447a712a
Teri <teri@bernardtechpartners.com>
2017-07-21 15:47:22 -0400

    header list-items show corresponding component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
- 
+ loadedFeature = 'shopping-list';
+
+onNavigate(selectedFeature: string) {
+  this.loadedFeature = selectedFeature;
+}
+
 }
