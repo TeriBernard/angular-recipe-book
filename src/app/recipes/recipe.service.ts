f2c0136cf755c9bd1b0f4ba302afdd80be41783f
Teri <teri@bernardtechpartners.com>
2017-08-10 12:39:10 -0400

    adding ingredients to recipe-detail

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,13 +1,28 @@
 import {EventEmitter} from '@angular/core';
 import {Recipe} from './recipe.model';
+import { Ingredient } from './../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
+    
 
     private recipes: Recipe[] = [
-        new Recipe('Pesto', 'Italian Green Goodness', 'https://cdn.pixabay.com/photo/2017/03/17/18/13/pesto-2152321__340.jpg'),
-        new Recipe('Test Recipe', 'This is just a test', 'https://cdn.pixabay.com/photo/2017/03/10/15/15/lime-2133091_960_720.jpg')
-      ];
+        new Recipe(
+            'Pesto', 
+            'Classic Italian sauce', 
+            'https://cdn.pixabay.com/photo/2017/03/17/18/13/pesto-2152321__340.jpg',
+            [   new Ingredient('Basil', 3),
+                new Ingredient('Garlic', 1)]),
+
+        new Recipe(
+            'Scrambled Eggs', 
+            'Basic breakfast food', 
+            'https://upload.wikimedia.org/wikipedia/commons/1/1e/Brinner.jpg',
+            [
+                new Ingredient('Eggs', 2),
+                new Ingredient('Butter', 1)
+            ])
+    ];
 
     getRecipes() {
         return this.recipes.slice();
